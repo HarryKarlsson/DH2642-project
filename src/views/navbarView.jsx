e802445f822c75cd143edc82a9f6272cb732a26e
Madeleine Frey <mfrey@kth.se>
2024-12-05 10:30:13 +0100

    Home och Navbar
    setup för navbar och home.

---
+++ src/views/navbarView.jsx
@@ -0,0 +1,10 @@
+import "../css/nav.css"
+function NavbarView() {
+    return (
+        <nav className="navBar">
+            <h1>Navbar Fungerar</h1>
+        </nav>
+    );
+}
+
+export default NavbarView;
--- src/vuejs/index.jsx
+++ src/vuejs/index.jsx
@@ -1,4 +1,8 @@
-import {createApp} from "vue";
-function App(){ return <div>hello world</div>; };
-const app= createApp(App);
+import { createApp } from "vue";
+import VueRoot from "../vuejs/VueRoot"; // Importera VueRoot-komponenten
+
+// Skapa Vue-applikationen med VueRoot som huvudkomponent
+const app = createApp(VueRoot);
+
+// Montera applikationen till div med id "root" i index.html
 app.mount("#root");
