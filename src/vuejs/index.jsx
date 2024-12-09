5adb760c89c004a78ef2021669503a8a0b6b96d4
sam <sam.maltin@gmail.com>
2024-12-09 11:21:48 +0100

    practice_view

--- src/vuejs/index.jsx
+++ src/vuejs/index.jsx
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import VueRoot from "../vuejs/VueRoot"; 
-
+import router from "../router";
 
 const app = createApp(VueRoot);
-
+app.use(router);
 app.mount("#root");
