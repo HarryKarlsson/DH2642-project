b69d6be70420da2581c2d60f87d430295cf332cc
Amanda Falk <amafal@kth.se>
2024-12-17 14:13:59 +0100

    Update firebaseApp.js

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,7 +1,7 @@
+//firebaseApp.js
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseConfig";
 
-// Initialize the Firebase app once
 const app = initializeApp(firebaseConfig);
 
 export {app};
