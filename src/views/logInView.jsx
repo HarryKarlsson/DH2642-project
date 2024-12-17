e6ca2cbd22af086faca6c0f2a7711576479af28b
Amanda Falk <amafal@kth.se>
2024-12-17 19:50:21 +0100

    Update in logInView.jsx

--- src/views/logInView.jsx
+++ src/views/logInView.jsx
@@ -1,49 +1,7 @@
-// logInView.jsx
-/*import { logIn } from "../authService";
-import "../css/logIn.css";
-import { useState } from "vue-router"
-
-function LogInView() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    async function handleLogIn() {
-        try {
-            await logIn(email, password); //using the logIn function from authService
-            console.log("Login successful");
-            window.location.hash = "#/welcome"; // Redirect to Welcome page
-        } catch (error) {
-            console.error("Error logging in:", error.message);
-            alert("Login failed: " + error.message);
-        }
-    }
-    return (
-        <div className="login-container">
-            <h1>Log In Quiz</h1>
-            <div>
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Enter Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button onClick={handleLogIn}>Log In</button>
-            </div>
-        </div>
-    );
-};
-
-export default LogInView;
-
-*/
-
+//logInView.jsx
 import { ref } from "vue";
 import { logIn } from "../authService";
-import "../css/welcome.css";
+import "../css/logIn.css";
 
 function LoginView() {
     const email = ref("");
